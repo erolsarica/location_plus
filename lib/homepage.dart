fe17dfad0f4b5cfc5217583b18b918c5bee5d423
Erol SARICA <erol.sarica@boun.edu.tr>
2024-01-06 21:42:52 +0300

    Update dependencies and add new screens

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'screens/current_location_screen.dart';
+import 'screens/nearby_places_screen.dart';
+import 'screens/polyline_screen.dart';
+import 'screens/search_places_screen.dart';
+import 'screens/simple_map_screen.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, world!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Google Maps"),
+        centerTitle: true,
+      ),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                    return const SimpleMapScreen();
+                  }));
+                },
+                child: const Text("Simple Map")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                    return const CurrentLocationScreen();
+                  }));
+                },
+                child: const Text("User current location")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                    return const SearchPlacesScreen();
+                  }));
+                },
+                child: const Text("Search Places")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                    return const NearByPlacesScreen();
+                  }));
+                },
+                child: const Text("Near by Places")),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                    return const PolylineScreen();
+                  }));
+                },
+                child: const Text("Polyline between 2 points"))
+          ],
         ),
       ),
     );
